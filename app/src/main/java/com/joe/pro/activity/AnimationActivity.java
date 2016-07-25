694e1937be43e1d17f155bbfd1f13585b7a374ec
Joe <qiaorz@qq.com>
2016-07-25 10:46:47 +0800

    SVG 动画

--- app/src/main/java/com/joe/pro/activity/AnimationActivity.java
+++ app/src/main/java/com/joe/pro/activity/AnimationActivity.java
@@ -1,9 +1,12 @@
 package com.joe.pro.activity;
 
 import android.app.Activity;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.joe.pro.R;
+import com.joe.pro.Tool.ViewTools;
 
 /**
  * Created by qiaorongzhu on 2016/7/21.
@@ -13,5 +16,7 @@ public class AnimationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+        ImageView ivAnim = ViewTools.findView(this, R.id.path_anim);
+        ((Animatable)ivAnim.getDrawable()).start();
     }
 }
